67d0be3fc46a07c739e358ebeb32c6c37bb7533d
Tomoyuki Nakatsuka <shouyu7144@gmail.com>
2014-09-09 10:08:25 +0900

    Delete before_migaration.rb

--- deploy/before_migaration.rb
+++
@@ -1,8 +0,0 @@
-Chef::Log.info("Running deploy/before_migrate.rb in myapp app...")
-current_release = release_path
-
-execute "rake assets:precompile" do
-  cwd current_release
-  command "bundle exec rake assets:precompile"
-  environment "RAILS_ENV" => 'production'
-end
